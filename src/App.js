0ed68c16da8c015f5352f5d049e232e0f93c02b7
lindatop <lindachristian90@gmail.com>
2022-08-24 23:22:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail';
+import logo from './assest/logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+      <Header logo={logo}/>
+      <Routes>
+        <Route path = '/'element={<Profile userName='lindatop' />}
+        />
+<Route path = '/projects' element={<Projects userName='lindatop'/>} />
+<Route path='/Projects/:name' element ={<ProjectDetail userName='lindatop' />}
+        />
+        </Routes>
+        </BrowserRouter>      
     </div>
   );
 }
